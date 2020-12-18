8164b39cfd7497f7fa08e4e3fbb5649f7424a16a
Manojkumar Ravi <manoj.kumarr@lntinfotech.com>
2020-12-18 11:43:42 +0530

    Updated

--- SampleAzure/Controllers/HomeController.cs
+++ SampleAzure/Controllers/HomeController.cs
@@ -22,7 +22,7 @@ namespace SampleAzure.Controllers
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Im recreating";
 
             return View();
         }
